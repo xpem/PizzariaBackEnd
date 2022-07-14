e405bc4fcd236431cabaecab9202524f8ce297e4
emanuel <emanuel@enotas.com.br>
2022-07-14 14:58:46 -0300

    jwt token session

--- src/services/user/AuthUserService.ts
+++ src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface AuthRequest {
   email: string;
@@ -16,11 +17,18 @@ class AuthUserService {
 
     const passwordMatch = await compare(password, user.password);
 
-    if(!passwordMatch){
-        throw new Error("User/Password incorrect");
+    if (!passwordMatch) {
+      throw new Error("User/Password incorrect");
     }
 
-    return { ok: true };
+    //"strict": false no ts config para utilização de  process.env. sem erros
+    const token = sign(
+      { name: user.name, email: user.email },
+      process.env.JWT_SECRET,
+      { subject: user.id, expiresIn: "30d" }
+    );
+
+    return { id: user.id, name: user.name, email: user.email, token: token };
   }
 }
 export { AuthUserService };
