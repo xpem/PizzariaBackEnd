1ef128f63a868e4982f2fb1f47c0fb3c6842115f
emanuel <emanuel@enotas.com.br>
2022-08-17 14:14:02 -0300

    list products

--- src/services/product/ListProductService.ts
+++ src/services/product/ListProductService.ts
@@ -2,7 +2,16 @@ import prismaClient from "../../prisma";
 
 class ListProductService {
   async execute() {
-    const res = await prismaClient.product.findMany();
+    const res = await prismaClient.product.findMany({
+      select: {
+        id: true,
+        price: true,
+        description: true,
+        name: true,
+        category: { select: { id: true, name: true } },
+      },
+      orderBy: { category_id: "desc" },
+    });
 
     return res;
   }
